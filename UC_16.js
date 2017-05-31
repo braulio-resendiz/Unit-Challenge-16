1b5f1385fd2258cb5638ad2c913f57d699d133be
Braulio  Resendiz Cordova <br19777@srcs.loc>
2017-05-31 09:30:00 -0700

    staring code

---
+++ UC_16.js
@@ -0,0 +1,20 @@
+function sendRequest() {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            // We got a response from the server!
+            if(this.status === 200) {
+                // The request was successful!
+                displayResponseData(this.responseText);
+            } else {
+                // There was a problem with the request.
+                // For example, the response may have a 404 (Not Found)
+                // or 500 (Internal Server Error) response code.
+            }
+        } else {
+            // Waiting for a response...
+        }
+    };
+    httpRequest.open("GET", "http://swapi.co/api/people/1/", true);
+    httpRequest.send();
+}
