43a3ddeeb2adb6190f818dda6b0882a717c1941c
braulio-resendiz <brauliocordova3@gmail.com>
2017-06-04 13:18:12 -0700

    character function

--- UC_16.js
+++ UC_16.js
@@ -3,23 +3,54 @@ function sendRequest() {
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
-            if(this.status === 200) {
+            if (this.status === 200) {
                 // The request was successful!
                 displayResponseData(this.responseText);
-            } else {
+            }
+            else {
                 // There was a problem with the request.
                 // For example, the response may have a 404 (Not Found)
                 // or 500 (Internal Server Error) response code.
             }
-        } else {
+        }
+        else {
             // Waiting for a response...
         }
     };
     httpRequest.open("GET", "http://swapi.co/api/", true);
     httpRequest.send();
 }
+
 function displayResponseData(data) {
     document.getElementById("output1").innerHTML = data;
 }
-
 // 88 people to put into system, http http://swapi.co/api/people/1/
+function findCharacter(SWId) {
+    // First get the zip code from the HTML textbox
+    var zip = document.getElementById(SWId).value;
+    // Now make a HTTP request
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            // We got a response from the server!
+            if (this.status === 200) {
+                // The request was successful!
+                displayPlace(this.responseText);
+            }
+            else if (this.status === 404) {
+                // No postal code found
+                displayPlace('{ "country" : "none" }');
+            }
+            else {
+                console.log("We have a problem...server responded with code: " + this.status);
+            }
+        }
+        else {
+            // Waiting for a response...
+        }
+    };
+    // URL for star wars character
+    var url = "http://swapi.co/api/people/" + number / ;
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
